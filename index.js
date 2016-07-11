c88526cffbb62cd27faedcddcf279a946bc6de6e
Johan Preynat <johan.preynat@gmail.com>
2016-07-11 11:48:41 +0200

    Replace escapeHTMLTags() by escapeHTMLEntities() method to fully escape HTML in text nodes

--- index.js
+++ index.js
@@ -123,13 +123,18 @@ function bfsOrder(node) {
  * Escape HTML tags from text nodes
  * There shouldn't be any HTML left in those nodes
  */
-function escapeHTMLTags(str) {
+function escapeHTMLEntities(str) {
   // Detect HTML and XML tags (i.e. namespaced) with attributes
-  var complexTags = /<(\/?[a-zA-Z]*(?:\:[a-zA-Z]*)?(?:\s[a-zA-Z]*(?:\:[a-zA-Z]*)?\=\".*?\")*\s?\/?)>/g;
+  // var complexTags = /<(\/?[a-zA-Z]*(?:\:[a-zA-Z]*)?(?:\s[a-zA-Z]*(?:\:[a-zA-Z]*)?\=\".*?\")*\s?\/?)>/g;
 
-  return str.replace(complexTags, function(tag, content) {
-    return '&lt;'+content+'&gt;';
-  });
+  // return str.replace(complexTags, function(tag, content) {
+  //   return '&lt;'+content+'&gt;';
+  // });
+  return str.replace(/&/g, '&amp;')
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#039;');
 }
 
 /*
@@ -142,7 +147,7 @@ function getContent(node) {
       text += node.childNodes[i]._replacement;
     }
     else if (node.childNodes[i].nodeType === 3) {
-      text += escapeHTMLTags(node.childNodes[i].data);
+      text += escapeHTMLEntities(node.childNodes[i].data);
     }
     else { continue; }
   }
